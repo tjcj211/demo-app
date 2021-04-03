6194db5879bc23ddabf2468753a6c9a06fcc96ff
tjcj211 <timcarta@hotmail.com>
2021-04-03 11:51:49 -0400

    LS17.9 Adding Links to people table.

--- src/components/tableBody.jsx
+++ src/components/tableBody.jsx
@@ -1,38 +1,43 @@
-import React, { Component } from "react";
-import Like from "./like";
+import React, { Component } from 'react';
+import Like from './like';
+import { Link } from 'react-router-dom';
 class TableBody extends Component {
-  render() {
-    const { people } = this.props; //this.props.people
-    return (
-      <tbody>
-        {people.map((person, index) => (
-          <tr key={index}>
-            <td>{person.name}</td>
-            <td>{person.job}</td>
-            <td>
-              <button
-                onClick={() => {
-                  this.props.onDelete(person);
-                }}
-                className="btn btn-danger btn-sm"
-              >
-                Delete
-              </button>
-            </td>
-            <td>
-              {" "}
-              <Like
-                liked={person.liked}
-                onClick={() => {
-                  this.props.onLike(person);
-                }}
-              ></Like>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    );
-  }
+	render() {
+		const { people } = this.props; //this.props.people
+		return (
+			<tbody>
+				{people.map((person, index) => (
+					<tr key={index}>
+						<td>
+							<Link to={`/people/${person.id}`}>
+								{person.name}
+							</Link>
+						</td>
+						<td>{person.job}</td>
+						<td>
+							<button
+								onClick={() => {
+									this.props.onDelete(person);
+								}}
+								className="btn btn-danger btn-sm"
+							>
+								Delete
+							</button>
+						</td>
+						<td>
+							{' '}
+							<Like
+								liked={person.liked}
+								onClick={() => {
+									this.props.onLike(person);
+								}}
+							></Like>
+						</td>
+					</tr>
+				))}
+			</tbody>
+		);
+	}
 }
 
 export default TableBody;
