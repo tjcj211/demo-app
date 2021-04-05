603172ddedbaeac3dd2c02c1088a50b7c1f33966
tjcj211 <timcarta@hotmail.com>
2021-04-05 14:38:08 -0400

    LS18.2 Prevent client from getting all info when form submitted.

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,29 +1,35 @@
 import React, { Component } from 'react';
 class LoginForm extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {};
-	}
+	state = {
+		account: {
+			email: '',
+			password: '',
+		},
+	};
 	handleSubmit = (e) => {
 		e.preventDefault();
-		console.log('Submitted');
+		console.log(this.state.email + ' ' + this.state.password);
 	};
 	render() {
 		return (
 			<div>
 				<form onSubmit={this.handleSubmit}>
 					<div className="form-group">
-						<label for="email">Email Address</label>
+						<label htmlFor="email">Email Address</label>
 						<input
 							type="email"
+							name="email"
 							className="form-control"
+							value={this.state.account.email}
 							id="email"
 							aria-describedby="emailHelp"
 						/>
 						<div className="form-group">
-							<label for="password">Password</label>
+							<label htmlFor="password">Password</label>
 							<input
 								type="password"
+								name="password"
+								value={this.state.account.password}
 								className="form-control"
 								id="password"
 							/>
