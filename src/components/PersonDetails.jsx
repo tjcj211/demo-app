4f31291567be2451ead6b19b06651ccb23cdc984
tjcj211 <timcarta@hotmail.com>
2021-04-07 14:43:18 -0400

    LS19.4 rudamentary updating people implemented

--- src/components/PersonDetails.jsx
+++ src/components/PersonDetails.jsx
@@ -14,10 +14,27 @@ class PersonDetails extends Component {
 		this.setState({ person });
 	}
 
+	handleUpdate = async (curPerson) => {
+		curPerson.name = 'Updated';
+		const { data: person } = await axios.put(
+			apiEndpoint + '/' + curPerson.id,
+			curPerson
+		);
+		this.setState({ person });
+	};
+
 	render() {
 		const { person } = this.state;
 		return (
 			<div>
+				<button
+					className="btn btn-primary"
+					onClick={() => {
+						this.handleUpdate(person);
+					}}
+				>
+					Update
+				</button>
 				<h3>Person Details</h3>
 				<div>Id: {person.id} </div>
 				<div>Name: {person.name} </div>
